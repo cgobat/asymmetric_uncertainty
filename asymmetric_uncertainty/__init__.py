b9a31ae1c344a500c75edafce7164a8e23abe240
Caden Gobat <36030084+cgobat@users.noreply.github.com>
2023-08-02 23:19:58 -0600

    Fix dependency installation order issue (#7)
    * Replace metadata with copyright/license notice
    * Use literals for metadata declaration in setup.py
    * Bump version

--- asymmetric_uncertainty/__init__.py
+++ asymmetric_uncertainty/__init__.py
@@ -1,8 +1,3 @@
 # package initialization
 
-from .core import (__author__,
-                   __author_affiliation__,
-                   __contact__,
-                   __deprecated__,
-                   __version__)
 from .core import *
--- setup.py
+++ setup.py
@@ -1,20 +1,17 @@
 from setuptools import setup, find_packages
-from asymmetric_uncertainty import __author__, __contact__, __version__
 
 with open("./README.md","r") as f:
     readme = f.read()
 
-with open("./LICENSE","r") as f:
-    license = f.read()
-
 if __name__ == '__main__':
     setup(name="asymmetric_uncertainty",
-          version=__version__,
-          author=__author__,
-          author_email=__contact__,
+          version="0.2.2",
+          author="Caden Gobat",
+          author_email="cgobat@gwu.edu",
           packages=find_packages(),
           url="https://github.com/cgobat/asymmetric_uncertainty",
           description="A package that implements a class to represent numeric quantities with asymmetric uncertainties.",
           long_description=readme,
-          license=license,
-          install_requires=["numpy","matplotlib"])
+          download_url="https://github.com/cgobat/asymmetric_uncertainty/archive/main.zip",
+          license="GPL-3.0",
+          install_requires=["numpy", "matplotlib"])
