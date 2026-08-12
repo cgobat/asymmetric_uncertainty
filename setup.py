46805ecd9814cb22d05caf4448a3e9c4a119e85b
Caden Gobat <36030084+cgobat@users.noreply.github.com>
2026-08-12 09:16:49 -0700

    Bump version to 0.2.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("./README.md", "r") as f:
 if __name__ == "__main__":
     setup(
         name="asymmetric_uncertainty",
-        version="0.2.4",
+        version="0.2.5",
         author="Caden Gobat",
         author_email="cgobat@gwu.edu",
         packages=find_packages(),
