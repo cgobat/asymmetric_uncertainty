e0b45728f88b4c3844421457eddaa42a4ee90452
Caden Gobat <36030084+cgobat@users.noreply.github.com>
2026-08-12 09:38:46 -0700

    Require NumPy 2+

--- setup.py
+++ setup.py
@@ -4,14 +4,19 @@ with open("./README.md", "r") as f:
     readme = f.read()
 
 if __name__ == "__main__":
-    setup(name="asymmetric_uncertainty",
-          version="0.2.4",
-          author="Caden Gobat",
-          author_email="cgobat@gwu.edu",
-          packages=find_packages(),
-          url="https://github.com/cgobat/asymmetric_uncertainty",
-          description="A package that provides a class for representing numeric quantities with asymmetric uncertainties.",
-          long_description=readme,
-          download_url="https://github.com/cgobat/asymmetric_uncertainty/archive/main.zip",
-          license="GPL-3.0",
-          install_requires=["numpy", "matplotlib"])
+    setup(
+        name="asymmetric_uncertainty",
+        version="0.2.4",
+        author="Caden Gobat",
+        author_email="cgobat@gwu.edu",
+        packages=find_packages(),
+        url="https://github.com/cgobat/asymmetric_uncertainty",
+        description="A package that provides a class for representing numeric quantities with asymmetric uncertainties.",
+        long_description=readme,
+        download_url="https://github.com/cgobat/asymmetric_uncertainty/archive/main.zip",
+        license="GPL-3.0",
+        install_requires=[
+            "numpy>=2",
+            "matplotlib",
+            ]
+        )
